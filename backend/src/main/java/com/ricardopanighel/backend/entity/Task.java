1a55ef626b30d80bfeb9bb385ac922febea62b45
ricardopanighel <ricardopanighellazarini@gmail.com>
2025-12-13 19:14:39 -0300

    feat: create Task entity with JPA mappings

---
+++ backend/src/main/java/com/ricardopanighel/backend/entity/Task.java
@@ -0,0 +1,55 @@
+package com.ricardopanighel.backend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tasks") // PostgreSQL table name
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;  // Unique identifier
+
+    private String title;    // Task title
+    private String description; // Task description
+    private boolean completed;  // status
+
+    // Empty constructor necessary to JPA
+    public Task() {}
+
+    // Constructor
+    public Task(String title, String description, boolean completed) {
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    //Getters and Setters
+    public Long getId(){
+        return id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getDescription(){
+        return descriptions;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public Boolean isCompleted(){
+        return completed;
+    }
+
+    public void setCompleted(boolean completed){
+        this.completed = completed;
+    }
+}
