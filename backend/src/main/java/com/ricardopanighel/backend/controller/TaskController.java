62f5eca8ede55b24e9d6c6724fae4494d1c4fc28
ricardopanighel <ricardopanighellazarini@gmail.com>
2025-12-16 00:35:31 -0300

    refactor: standardize HTTP responses using ResponseEntity in TaskController

--- backend/src/main/java/com/ricardopanighel/backend/controller/TaskController.java
+++ backend/src/main/java/com/ricardopanighel/backend/controller/TaskController.java
@@ -22,38 +22,46 @@ public class TaskController {
     }
 
     @GetMapping
-    public List<TaskResponse> getAllTasks() {
+    public ResponseEntity<List<TaskResponse>> getAllTasks() {
         List<Task> tasks = taskService.findAll();
-        return tasks.stream()
+        List<TaskResponse> list = tasks.stream()
                 .map(task -> {
                     return TaskMapper.toTaskResponse(task);
                 })
                 .toList();
+
+        return ResponseEntity.ok(list);
     }
 
     @GetMapping("/{id}")
-    public TaskResponse getTaskById(@PathVariable Long id) {
+    public ResponseEntity<TaskResponse> getTaskById(@PathVariable Long id) {
         Task task = taskService.findTaskById(id)
                 .orElseThrow(() -> new RuntimeException("Task not found."));
 
-        return TaskMapper.toTaskResponse(task);
+        TaskResponse taskResponse = TaskMapper.toTaskResponse(task);
+
+        return ResponseEntity.ok(taskResponse);
     }
 
     @PostMapping
-    public TaskResponse createTask(@RequestBody TaskRequest request) {
+    public ResponseEntity<TaskResponse> createTask(@RequestBody TaskRequest request) {
         Task newTask = TaskMapper.toTask(request);
         Task savedTask = taskService.saveTask(newTask);
-        return TaskMapper.toTaskResponse(savedTask);
+        TaskResponse taskResponse = TaskMapper.toTaskResponse(savedTask);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskResponse);
     }
 
     @PutMapping("/{id}")
-    public TaskResponse updateTask(@PathVariable Long id, @RequestBody TaskRequest requestDetails) {
+    public ResponseEntity<TaskResponse> updateTask(@PathVariable Long id, @RequestBody TaskRequest requestDetails) {
         Task task = taskService.findTaskById(id)
                 .orElseThrow(() -> new RuntimeException("Task not found."));
 
         TaskMapper.updateTask(task, requestDetails);
         Task savedUpdate = taskService.saveTask(task);
-        return TaskMapper.toTaskResponse(savedUpdate);
+        TaskResponse taskResponse = TaskMapper.toTaskResponse(savedUpdate);
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(taskResponse);
     }
 
     @DeleteMapping("/{id}")
